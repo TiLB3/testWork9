12a1c1b2750ad1f2d0342e49d7c23bedfa0a6a30
Talipov Timurlan <taltima4@gmail.com>
2026-02-07 12:20:21 +0600

    Создал главный store,добавил роуты на главную страницу и на несуществующую,создал кастомные хуки для dispatch,useSelector.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import {Home} from "@mui/icons-material";
 
 const App = () => {
 
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
     </>
   )
 };
